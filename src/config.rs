633f4c397235f3c6d139be9fb9d5b90c20ec9c8c
Olly Swanson <olly.swanson95@gmail.com>
2021-04-07 23:39:33 +0200

    Config: add Config type parse from UncheckedConfig

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,7 @@
 use crate::notifications::Notification;
 use serde::Deserialize;
 use std::collections::HashMap;
+use std::io::BufRead;
 use url::Url;
 
 // TODO: Find a better home for me.
@@ -18,13 +19,40 @@ struct UncheckedConfig {
 }
 
 pub struct Config {
-    pub services: &'static HashMap<String, Service>,
-    pub notifications: &'static HashMap<String, Notification>,
+    pub services: HashMap<String, Service>,
+    pub notifications: HashMap<String, Notification>,
+}
+
+impl Config {
+    // TODO: Replace the boxed error with something more helpful
+    pub fn new<R: BufRead>(reader: R) -> Result<Self, Box<dyn std::error::Error>> {
+        let unchecked = serde_yaml::from_reader(reader)?;
+
+        Self::parse(unchecked)
+    }
+
+    fn parse(unchecked: UncheckedConfig) -> Result<Self, Box<dyn std::error::Error>> {
+        for (name, service) in unchecked.services.iter() {
+            if service
+                .notifications
+                .iter()
+                .any(|notification| unchecked.notifications.get(notification).is_none())
+            {
+                return Err(format!("Service: {} has undefined notification", name))?;
+            }
+        }
+
+        Ok(Self {
+            services: unchecked.services,
+            notifications: unchecked.notifications,
+        })
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use std::io::Cursor;
 
     #[test]
     fn should_deserialize_unchecked() {
@@ -48,7 +76,9 @@ mod test {
                     url: "http://localhost:3001/slack"
             "#;
 
-        let config: UncheckedConfig = serde_yaml::from_str(yaml).unwrap();
+        let cursor = Cursor::new(yaml.as_bytes());
+        let config = Config::new(cursor).unwrap();
+
         let s1 = Service {
             url: Url::parse("http://localhost:3000/health_check").unwrap(),
             interval: 2,
@@ -63,5 +93,24 @@ mod test {
 
         assert_eq!(&s1, config.services.get("s1").unwrap());
         assert_eq!(&s2, config.services.get("s2").unwrap());
+    }
+
+    #[test]
+    fn should_be_err_if_undefined_notfication() {
+        let yaml = r#"
+            services:
+                s1:
+                    url: "http://localhost:3000/health_check"
+                    interval: 2
+                    notifications: [n1, n2]
+
+            notifications:
+                n1:
+                    type: slack
+                    url: "http://localhost:3000/slack"
+            "#;
+        let cursor = Cursor::new(yaml.as_bytes());
+
+        assert!(Config::new(cursor).is_err());
     }
 }
